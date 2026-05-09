1dd2cd52b8dc72e39e15ebce89c5eef8cee67ea6
supakan1223-ui <Supakan1223@gmail.com>
2026-05-09 16:21:22 +0800

    30 is so hard

--- A030.cpp
+++ A030.cpp
@@ -1,21 +1,46 @@
 #include<iostream>
 #include<string>
+#include<vector>
+#include <sstream>
 using namespace std;
 int main()
 {
     int n ;
     cin >> n ;
     cin.ignore() ;
-    string iput;
-    getline(cin, iput);
-    string desp = "" ;
-    for(int de = 0 ; de < iput.size() ; de++)
+    string line;
+    getline(cin, line);
+    stringstream ss(line);
+    vector<int> input ;
+    int box ;
+    int set[n] = {};
+    while(ss >> box)
     {
-        if(iput[de] != ' ')
-        {
-            desp += iput[de] ;
-        }
+        input.push_back(box) ;
     }
-    int enco = stoi(desp) ;
-    cout << enco ;
+    int i = 0 ;
+    int result = 0;
+    for(int a = 0 ; a < n  ; a ++)
+    {
+        if(input[i] > input[i+1])
+        set[a] = input[i] ;
+        else if (input[i] < input[i+1])
+        set[a] = input[i+1] ;
+        else if (input[i] == input[i+1])
+        set[a] = input[i+1] ;
+        i += 2 ;
+    }
+    for(int a = 0 ; a < n  ; a ++)
+        result += set[a] ;
+    for(int a = 0 ; a < n - 1 ; a ++)
+    {
+        if (n > 1)
+        {
+            cout <<set[a] << " + " ;
+        } 
+    }
+    if(n == 1)
+    cout << set[0] ;
+    else if(n > 1)
+    cout << set[n-1] << " = " << result ;
 }
