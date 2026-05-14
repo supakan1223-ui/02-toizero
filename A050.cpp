8130663a5e9c9df725103ad02d42693c1e821577
supakan1223-ui <supakan1223@gmail.com>
2026-05-14 15:11:55 +0700

    50.fix

--- A050.cpp
+++ A050.cpp
@@ -1,67 +1,24 @@
-#include "raylib.h"
-
-int main () {
-    const int screenWidth = 850;
-    const int screenHeight = 450;
-    InitWindow(screenWidth, screenHeight, "apple eater game ");
-
-    Texture2D PlayerTexture = LoadTexture("worm01.png");
-
-    float posX = screenWidth / 2;
-    float posY = screenHeight / 2;
-    float speed = 5.0f;
-
-    int score = 0;
-    int AppleSize = 30; //คะแนนและแอปเปิ้ล
-
-    float appleX = GetRandomValue(0, screenWidth - AppleSize);
-    float appleY = GetRandomValue(0, screenHeight - AppleSize); //สุ่มตำแหน่งแรกของแอปเปิ้ล
-
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose()) {
-        if(IsKeyDown(KEY_W)) posY -= speed;
-        if(IsKeyDown(KEY_A)) posX -= speed;
-        if(IsKeyDown(KEY_S)) posY += speed;
-        if(IsKeyDown(KEY_D)) posX += speed;
-
-        if (posX < 0) posX = 0; //กันชนขอบจอซ้าย
-        if (posY < 0) posY = 0; //กันชนขอบจอบน
-        if (posX > screenWidth - PlayerTexture.width) posX = screenWidth - PlayerTexture.width; //กันชนขอบจอขวา
-        if (posY > screenHeight - PlayerTexture.height) posY = screenHeight - PlayerTexture.height; //กันชนขอบจอล่าง
-
-        //ระบบกินแอปเปิ้ล
-        Rectangle playerBox = { posX, posY,(float)PlayerTexture.width,(float)PlayerTexture.height };
-        Rectangle appleBox = { appleX, appleY, (float)AppleSize, (float)AppleSize };
-
-        //ถ้ากล่องสองอันชนกัน = กิน
-
-        if (CheckCollisionRecs(playerBox,appleBox)) {
-            score += 10;
-
-            //สุ่มตำแหน่งใหม่
-
-            appleX = GetRandomValue(0, screenWidth - AppleSize);
-            appleY = GetRandomValue(0, screenHeight - AppleSize);
-
-            //การวาดผลลงจอ
-
-            BeginDrawing();
-                ClearBackground(BLACK);
-
-                DrawRectangle(appleX, appleY, AppleSize, AppleSize, RED);
-
-                DrawTexture(PlayerTexture, posX, posY, WHITE);
-
-                DrawText(TextFormat("SCORE:%d",score), 20, 20, 30, GREEN);
-
-            EndDrawing();
-
-        }
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+using namespace std;
+int main() {
+    vector<int> in ;
+    string line ;
+    getline(cin , line) ;
+    stringstream a(line) ;
+    int x ;
+    int numm = 0 ;
+    int numw = 0 ;
+    while(a >> x)
+    if(x >= 0)
+    in.push_back(x) ;
+    for(int a = 0; a < (int)in.size() ; a++) {
+      if( in[a] % 2 == 0)
+      numm += 1 ;
+      else if ( in[a] % 2 != 0)
+      numw += 1 ;
     }
-
-    UnloadTexture(PlayerTexture);
-    CloseWindow();
-    return 0;
-
+    cout << numw << " " << numm << " " << in.size();
 }
